2d22d9533828136d2d4f6551ce94a672280cf2fa
BEERESHBANAKARDS <88141192+BEERESHBANAKARDS@users.noreply.github.com>
2024-07-19 10:03:21 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Dingdi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001956199528'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mizotelegram')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
